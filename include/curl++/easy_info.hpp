375a0f25cc97a151c8e7f946effdea3d66ee098f
taiyu <taiyu.len@gmail.com>
2019-05-25 15:37:47 -0700

    add more easy info stuff

--- include/curl++/easy_info.hpp
+++ include/curl++/easy_info.hpp
@@ -5,7 +5,8 @@
 #include <chrono>
 
 namespace curl {
-namespace info {
+namespace info { // info types
+
 /** Base template type for options.
  * @param o the option value.
  * @param T the user facing argument type.
@@ -37,15 +38,35 @@ struct info<o, std::chrono::duration<Rep, Period>> {
 	}
 };
 
-//@{
-/// Curl info types
+} // namespace info
+namespace info { // info objects
+
 #define CURL_INFO_TYPE(NAME, TYPE) info<CURLINFO_##NAME, TYPE>{}
 constexpr auto url               = CURL_INFO_TYPE(EFFECTIVE_URL, std::string);
 // TODO make response code type that can stringify the response and whatnot.
 constexpr auto response_code     = CURL_INFO_TYPE(RESPONSE_CODE, long);
 constexpr auto http_connect_code = CURL_INFO_TYPE(HTTP_CONNECTCODE, long);
 constexpr auto http_version      = CURL_INFO_TYPE(HTTP_VERSION, long);
+
+constexpr auto redirect_count    = CURL_INFO_TYPE(REDIRECT_COUNT, long);
+constexpr auto redirect_url      = CURL_INFO_TYPE(REDIRECT_URL, std::string);
+// bytes
+constexpr auto size_upload       = CURL_INFO_TYPE(SIZE_UPLOAD_T, curl_off_t);
+constexpr auto size_download     = CURL_INFO_TYPE(SIZE_DOWNLOAD_T, curl_off_t);
+
+// bytes per second
+constexpr auto speed_upload      = CURL_INFO_TYPE(SPEED_UPLOAD_T, curl_off_t);
+constexpr auto speed_download    = CURL_INFO_TYPE(SPEED_DOWNLOAD_T, curl_off_t);
+
+constexpr auto header_size       = CURL_INFO_TYPE(HEADER_SIZE, long);
+constexpr auto request_size      = CURL_INFO_TYPE(REQUEST_SIZE, long);
+
+constexpr auto content_length_download = CURL_INFO_TYPE(CONTENT_LENGTH_DOWNLOAD_T, curl_off_t);
+constexpr auto content_length_upload = CURL_INFO_TYPE(CONTENT_LENGTH_UPLOAD_T, curl_off_t);
+
 constexpr auto content_type      = CURL_INFO_TYPE(CONTENT_TYPE, std::string);
+
+constexpr auto userdata          = CURL_INFO_TYPE(PRIVATE, void*);
 
 // TODO rather then a duration, a timepoint may be better
 constexpr auto filetime           = CURL_INFO_TYPE(FILETIME_T, std::chrono::seconds);
@@ -58,7 +79,7 @@ constexpr auto pretransfer_time   = CURL_INFO_TYPE(PRETRANSFER_TIME_T,   std::ch
 constexpr auto starttransfer_time = CURL_INFO_TYPE(STARTTRANSFER_TIME_T, std::chrono::microseconds);
 constexpr auto redirect_time      = CURL_INFO_TYPE(REDIRECT_TIME_T,      std::chrono::microseconds);
 #undef CURL_INFO_TYPE
-//@}
+
 } // namespace info
 } // namespace curl
 #endif // CURLPLUSPLUS_EASY_INFO_HPP
